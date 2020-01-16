2fb58cd24d49962c5947b9f25762d2ff0b7c2f64
emre cavunt <emre.cavunt@gmail.com>
2020-01-16 17:53:30 +0300

    2 model in a row

--- app/server.py
+++ app/server.py
@@ -10,11 +10,18 @@ import time
 from fastai import *
 from fastai.vision import *
 
-model_file_url = 'https://www.dropbox.com/s/2ndjs5lx90x0155/export.pkl?raw=1'
+
+model_file_url = 'https://www.dropbox.com/s/xcrxkd2fb6h9lr9/export.pkl?raw=1'
 model_file_name = 'export'
-classes = ['Inside', 'Side', 'Front','Unproper']
+classes = ['decay', 'missing', 'split']
 path = Path(__file__).parent
 
+
+
+kmodel_file_url = 'https://www.dropbox.com/s/2ndjs5lx90x0155/export.pkl?raw=1'
+kmodel_file_name = 'export'
+kclasses = ['Inside', 'Side', 'Front','Unproper']
+kpath = Path(__file__).parent
 
 app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
@@ -32,10 +39,21 @@ async def setup_learner():
     learn = load_learner(path)
     return learn
 
+
+async def ksetup_learner():
+    await download_file(kmodel_file_url, kpath/f'{kmodel_file_name}.pkl')
+    klearn = load_learner(kpath)
+    return klearn
+
 loop = asyncio.get_event_loop()
 tasks = [asyncio.ensure_future(setup_learner())]
 learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
 loop.close()
+
+kloop = asyncio.get_event_loop()
+ktasks = [asyncio.ensure_future(ksetup_learner())]
+klearn = loop.run_until_complete(asyncio.gather(*ktasks))[0]
+kloop.close()
 
 @app.route('/')
 def index(request):
@@ -59,6 +77,22 @@ async def analyze(request):
         return JSONResponse({'result': str(result.obj),'executiontime': dtexectime, 'probability':  str(prob)})
     except Exception as e:
         return JSONResponse({'error:',str(e)})
+
+
+@app.route('/analyze-pose', methods=['POST'])
+async def analyze(request):
+    try:
+        t = time.time() # get execution time
+        data = await request.form()
+        img_bytes = await (data['file'].read())
+        img = open_image(BytesIO(img_bytes))
+        result,_,prob = klearn.predict(img)
+        dt = time.time() - t
+        dtexectime = ("%0.03f seconds" % (dt))
+        return JSONResponse({'result': str(result.obj),'executiontime': dtexectime, 'probability':  str(prob)})
+    except Exception as e:
+        return JSONResponse({'error:',str(e)})
+
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app, host='0.0.0.0', port=8080)
--- app/static/client.js
+++ app/static/client.js
@@ -50,3 +50,40 @@ function analyze() {
   fileData.append("file", uploadFiles[0]);
   xhr.send(fileData);
 }
+
+function analyze_pose() {
+  var uploadFiles = el("file-input").files;
+  if (uploadFiles.length != 1) alert("Please select 1 file to analyze!");
+
+  el("analyze-button").innerHTML = "Analyzing...";
+  var xhr = new XMLHttpRequest();
+  var loc = window.location;
+  xhr.open(
+    "POST",
+    `${loc.protocol}//${loc.hostname}:${loc.port}/analyze-pose`,
+    true
+  );
+  xhr.onerror = function() {
+    alert(xhr.responseText);
+  };
+  xhr.onload = function(e) {
+    if (this.readyState === 4) {
+      var response = JSON.parse(e.target.responseText);
+      console.log(response);
+      if (!response.error) {
+        el(
+          "result--pose-label"
+        ).innerHTML = `Result = ${response["result"]} - exc.time: ${response["executiontime"]} - probability: ${response["probability"]}`;
+      } else {
+        el(
+          "result-pose-label"
+        ).innerHTML = `Result = ${response["result"]} - exc.time: ${response["executiontime"]} - probability: ${response["probability"]}`;
+      }
+    }
+    el("analyze-button").innerHTML = "Analyze";
+  };
+
+  var fileData = new FormData();
+  fileData.append("file", uploadFiles[0]);
+  xhr.send(fileData);
+}
